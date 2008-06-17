082c1a89ff7d4850e6069a71ce909214a728b273
Michael Schwab <mike.schwab@gmail.com>
2008-06-17 17:09:06 -0400

    highlighting leaves after you add text

--- public/javascripts/basicblog.js
+++ public/javascripts/basicblog.js
@@ -1,11 +1,18 @@
 function highlight_incomplete_form(elt) {
-	if ($(elt.target).val() == '') {
-      $(elt.target).parent().addClass('unfinished_form');
+	if ($(elt).val() == '') {
+      $(elt).parent().addClass('unfinished_form');
 	}
+}
+
+function remove_highlighting(elt) {
+	$(elt).parent().removeClass('unfinished_form');
 }
 
 $(document).ready(function() {
   $('.required').bind("blur", function(elt) {
-    highlight_incomplete_form(elt);
+    highlight_incomplete_form(elt.target);
+  })
+  $('.required').bind("change", function(elt) {
+	remove_highlighting(elt.target);
   })
 })
